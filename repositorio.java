9555ee3df9750adef85de5a38f4d4b89d12673db
Fr34k303 <111389064+Fr34k303@users.noreply.github.com>
2022-08-21 20:39:07 -0500

    Update repositorio.java

--- repositorio.java
+++ repositorio.java
@@ -2,5 +2,6 @@ public class repositorio{
   public static void main(String args[]){
 
    System.out.println("Bienvenidos al repositorio Java-Start"); 
+   System.out.println("Este es un saludo de Oscar para todxs")
  }
 }
